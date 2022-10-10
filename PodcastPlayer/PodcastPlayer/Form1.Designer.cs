f536b56908f39c9e37173f6400fe84568f52f4a8
noahjarvback <noah.jarvback@hotmail.com>
2022-10-10 14:06:18 +0200

    push label buttons

--- PodcastPlayer/PodcastPlayer/Form1.Designer.cs
+++ PodcastPlayer/PodcastPlayer/Form1.Designer.cs
@@ -35,13 +35,19 @@
             this.cbxUpdateFreq = new System.Windows.Forms.ComboBox();
             this.cbxCategory = new System.Windows.Forms.ComboBox();
             this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnNewPod = new System.Windows.Forms.Button();
+            this.btnSavePod = new System.Windows.Forms.Button();
+            this.btnDeletePod = new System.Windows.Forms.Button();
+            this.btnNewCategory = new System.Windows.Forms.Button();
+            this.btnSaveCategory = new System.Windows.Forms.Button();
+            this.btnDeleteCategory = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblCategories
             // 
             this.lblCategories.AutoSize = true;
             this.lblCategories.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblCategories.Location = new System.Drawing.Point(1099, 57);
+            this.lblCategories.Location = new System.Drawing.Point(1073, 56);
             this.lblCategories.Name = "lblCategories";
             this.lblCategories.Size = new System.Drawing.Size(143, 29);
             this.lblCategories.TabIndex = 0;
@@ -71,7 +77,7 @@
             // 
             this.lblCategory.AutoSize = true;
             this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblCategory.Location = new System.Drawing.Point(809, 308);
+            this.lblCategory.Location = new System.Drawing.Point(770, 308);
             this.lblCategory.Name = "lblCategory";
             this.lblCategory.Size = new System.Drawing.Size(119, 29);
             this.lblCategory.TabIndex = 3;
@@ -93,7 +99,7 @@
             this.cbxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cbxCategory.ForeColor = System.Drawing.SystemColors.ScrollBar;
             this.cbxCategory.FormattingEnabled = true;
-            this.cbxCategory.Location = new System.Drawing.Point(809, 352);
+            this.cbxCategory.Location = new System.Drawing.Point(770, 351);
             this.cbxCategory.Name = "cbxCategory";
             this.cbxCategory.Size = new System.Drawing.Size(242, 45);
             this.cbxCategory.TabIndex = 5;
@@ -109,11 +115,77 @@
             this.txtUrl.TabIndex = 6;
             this.txtUrl.Text = "http://";
             // 
+            // btnNewPod
+            // 
+            this.btnNewPod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNewPod.Location = new System.Drawing.Point(454, 431);
+            this.btnNewPod.Name = "btnNewPod";
+            this.btnNewPod.Size = new System.Drawing.Size(180, 54);
+            this.btnNewPod.TabIndex = 7;
+            this.btnNewPod.Text = "Ny...";
+            this.btnNewPod.UseVisualStyleBackColor = true;
+            // 
+            // btnSavePod
+            // 
+            this.btnSavePod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSavePod.Location = new System.Drawing.Point(652, 431);
+            this.btnSavePod.Name = "btnSavePod";
+            this.btnSavePod.Size = new System.Drawing.Size(164, 54);
+            this.btnSavePod.TabIndex = 8;
+            this.btnSavePod.Text = "Spara";
+            this.btnSavePod.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletePod
+            // 
+            this.btnDeletePod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDeletePod.Location = new System.Drawing.Point(837, 431);
+            this.btnDeletePod.Name = "btnDeletePod";
+            this.btnDeletePod.Size = new System.Drawing.Size(175, 54);
+            this.btnDeletePod.TabIndex = 9;
+            this.btnDeletePod.Text = "Ta bort...";
+            this.btnDeletePod.UseVisualStyleBackColor = true;
+            // 
+            // btnNewCategory
+            // 
+            this.btnNewCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNewCategory.Location = new System.Drawing.Point(1073, 351);
+            this.btnNewCategory.Name = "btnNewCategory";
+            this.btnNewCategory.Size = new System.Drawing.Size(150, 56);
+            this.btnNewCategory.TabIndex = 10;
+            this.btnNewCategory.Text = "Ny...";
+            this.btnNewCategory.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveCategory
+            // 
+            this.btnSaveCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSaveCategory.Location = new System.Drawing.Point(1242, 352);
+            this.btnSaveCategory.Name = "btnSaveCategory";
+            this.btnSaveCategory.Size = new System.Drawing.Size(150, 56);
+            this.btnSaveCategory.TabIndex = 11;
+            this.btnSaveCategory.Text = "Spara";
+            this.btnSaveCategory.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteCategory
+            // 
+            this.btnDeleteCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDeleteCategory.Location = new System.Drawing.Point(1407, 352);
+            this.btnDeleteCategory.Name = "btnDeleteCategory";
+            this.btnDeleteCategory.Size = new System.Drawing.Size(150, 56);
+            this.btnDeleteCategory.TabIndex = 12;
+            this.btnDeleteCategory.Text = "Ta bort...";
+            this.btnDeleteCategory.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(25F, 51F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1636, 696);
+            this.ClientSize = new System.Drawing.Size(1570, 870);
+            this.Controls.Add(this.btnDeleteCategory);
+            this.Controls.Add(this.btnSaveCategory);
+            this.Controls.Add(this.btnNewCategory);
+            this.Controls.Add(this.btnDeletePod);
+            this.Controls.Add(this.btnSavePod);
+            this.Controls.Add(this.btnNewPod);
             this.Controls.Add(this.txtUrl);
             this.Controls.Add(this.cbxCategory);
             this.Controls.Add(this.cbxUpdateFreq);
@@ -139,5 +211,11 @@
         private ComboBox cbxUpdateFreq;
         private ComboBox cbxCategory;
         private TextBox txtUrl;
+        private Button btnNewPod;
+        private Button btnSavePod;
+        private Button btnDeletePod;
+        private Button btnNewCategory;
+        private Button btnSaveCategory;
+        private Button btnDeleteCategory;
     }
 }
