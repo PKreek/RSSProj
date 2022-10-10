6856c943bcd07c1958c2f5294be74e7278f48abc
noahjarvback <noah.jarvback@hotmail.com>
2022-10-10 13:30:36 +0200

    Nytt

--- WinFormsApp1/PodcastPlayer/Program.cs
+++ WinFormsApp1/PodcastPlayer/Program.cs
@@ -16,7 +16,7 @@ namespace PodcastPlayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PodcastPlayer());
         }
     }
 }
