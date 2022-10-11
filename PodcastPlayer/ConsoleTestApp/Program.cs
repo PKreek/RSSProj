0d6c4623f7c257176a6952ec98067fa077829935
markwest7 <markus.westberg@gmail.com>
2022-10-11 13:03:56 +0200

    console feed

---
+++ PodcastPlayer/ConsoleTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess;
+using System.ServiceModel.Syndication;
+
+namespace ConsoleTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SyndicationFeed feed = FeedReader.ReadFeed("https://www.espn.com/espn/rss/news");
+            Console.WriteLine(feed.Title.Text);
+        }
+    }
+}
---
+++ PodcastPlayer/DataAccess/FeedReader.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace DataAccess
+{
+    public static class FeedReader
+    {
+        public static SyndicationFeed ReadFeed (string url)
+        {
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+            return feed;
+        }
+    }
+}
