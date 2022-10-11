f912debd3583987d65621c27d59c186a15754fbb
markwest7 <markus.westberg@gmail.com>
2022-10-11 12:42:53 +0200

    rss

--- PodcastPlayer/DataAccess/Class1.cs
+++ PodcastPlayer/DataAccess/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace DataAccess
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace DataAccess
 {
-    public class Class1
+    public static class Class1
     {
+        public static XmlReader reader = XmlReader.Create("https://www.espn.com/espn/rss/news");
+        public static SyndicationFeed feed = SyndicationFeed.Load(reader);
 
     }
 }
