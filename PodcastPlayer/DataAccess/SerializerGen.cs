ff8df167dc9e9f4a077966cd2b86bdd1373a36da
PKreek <93782423+PKreek@users.noreply.github.com>
2022-10-12 14:14:38 +0300

    Create SerializerGen.cs

---
+++ PodcastPlayer/DataAccess/SerializerGen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer
+{
+    internal class SerializerGen<T>
+    {
+        private string fileName;
+        public string FileName
+        {
+            set
+            {
+                fileName = value;
+            }
+        }
+        public SerializerGen(string fName)
+        {
+            FileName = fName;
+        }
+        public void Serialize(List<T> list)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (FileStream xmlOut =
+                new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                xmlSerializer.Serialize(xmlOut, list);
+            }
+        }
+
+        public List<T> Deserialize()
+        {
+            List<T> listan;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<T>));
+            using (FileStream xmlIn =
+                new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                listan = (List<T>)xmlSerializer.Deserialize(xmlIn);
+            }
+            return listan;
+        }
+    }
+}
