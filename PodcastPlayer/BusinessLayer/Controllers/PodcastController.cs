f5e0e29c3a86b8342293ea956d5924144883256b
PKreek <93782423+PKreek@users.noreply.github.com>
2022-10-17 18:38:23 +0300

    Controller

--- PodcastPlayer/BusinessLayer/Controllers/PodcastController.cs
+++ PodcastPlayer/BusinessLayer/Controllers/PodcastController.cs
@@ -5,13 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using Models;
 using DataAccessLayer;
+using DataAccessLayer.Repository;
 
 namespace BusinessLayer.Controllers
 {
     public class PodcastController
     {
         IRepository<Podcast> podcastRepository;
+        public PodcastController()
+        {
+            podcastRepository = new PodcastRepository();
+        }
 
+        //public void CreatePodcast(string episode, string name, double frequency, string category)
+        //{
+        //    Podcast podcastObj = new Podcast(episode, name, frequency, category);
+        //    podcastRepository.Insert(podcastObj);
+        //}
+        public List<Podcast> RetrieveAllPods()
+        {
+            return podcastRepository.GetAll();
+        }
 
     }
 }
