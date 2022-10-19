ad6e99d403159ae128af86cb0c254800b71dd22f
NikAntKar <niklas.karlsson12345@hotmail.com>
2022-10-19 13:11:22 +0200

    Category

--- PodcastPlayer/DataAccess/Repository/CategoryRepository.cs
+++ PodcastPlayer/DataAccess/Repository/CategoryRepository.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
+using System.Xml.Serialization;
+
 
 namespace DataAccessLayer.Repository
 {
-    internal class CategoryRepository
+    public class CategoryRepository : IRepository<Category>
     {
+        Serializer<Category> CategorySerializer;
+        List<Category> ListOfCategories;
+
+        public CategoryRepository()
+        {
+            ListOfCategories = new List<Category>();
+            CategorySerializer = new Serializer<Category>(nameof(ListOfCategories));
+        }
+
+        public List<Category> GetAll()
+        {
+            List<Category> listAvRegistreradeCategory = new List<Category>(); 
+            try
+            {
+                listAvRegistreradeCategory = CategorySerializer.Deserialize();
+            }
+            catch (Exception)
+            {
+
+            }
+            return listAvRegistreradeCategory;
+        }
+        public Category GetById(string id)
+        {
+            Category category = null;
+            foreach(var item in CategorySerializer.Deserialize())
+            {
+                if (item.CategoryId.Equals(id))
+                {
+                    category = item;
+                }
+            }
+            return category;
+        }
+        public void Insert ( Category theObject)
+        {
+            ListOfCategories.Add(theObject);
+            SaveChanges();
+        }
+        public void Update (int index, Category theNewObject)
+        {
+            if  (index >=0)
+            {
+                ListOfCategories[index] = theNewObject;
+            }
+        }
+        public void Delete (int index)
+        {
+            ListOfCategories.RemoveAt(index);
+            SaveChanges();
+        }
+
+        public void SaveChanges ()
+        {
+            CategorySerializer.Serialize(ListOfCategories);
+        }
+
+        public Category GetByID(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
