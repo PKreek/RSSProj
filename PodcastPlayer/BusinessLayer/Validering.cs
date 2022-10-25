fa5d7a60fd2b06476b0ad697150e304fce30f2ca
NikAntKar <niklas.karlsson12345@hotmail.com>
2022-10-25 13:51:19 +0200

    validering

--- PodcastPlayer/BusinessLayer/Validering.cs
+++ PodcastPlayer/BusinessLayer/Validering.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer
 {
-    internal class Validering
+    class Validering
     {
+
+        public Validering()
+        {
+
+        }
+
+        public bool isEmpty(string field)
+        {
+
+
+            bool result = false;
+
+            if (field == null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool toLong(string field)
+        {
+            bool result = false;
+            if(field.Length > 25)
+            {
+                result = true;
+            }
+
+            return result; 
+        }
+
     }
+
 }
